c5cfb9aa1ba0a3fe96039329b6c76a3c8840184d
Lisa Jongmans <ljongman@e-r9-p4.s19.be>
2019-07-22 18:56:57 +0200

    ft_iterative_factorial

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljongman <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/19 09:41:42 by ljongman          #+#    #+#             */
+/*   Updated: 2019/07/22 18:55:40 by ljongman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+int		ft_iterative_factorial(int nb)
+{
+	int answer;
+
+	answer = nb;
+	if (nb > 12 || nb < 0)
+		return (0);
+	if (nb == 0 || nb == 1)
+		return (1);
+	while (nb > 1)
+	{
+		answer = answer * (nb - 1);
+		nb--;
+	}
+	return (answer);
+}
+
+int		main(void)
+{
+	int nb;
+
+	nb = 6;
+	printf("%i\n", ft_iterative_factorial(nb));
+	return (0);
+}
