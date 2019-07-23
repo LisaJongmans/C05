a8aa40f0e0d8d3cbdf4d8b8a840fa403bfb6ed59
Lisa Jongmans <ljongman@e-r9-p4.s19.be>
2019-07-23 10:49:50 +0200

    ft_recursive_factorial

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: ljongman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/22 21:45:29 by ljongman          #+#    #+#             */
-/*   Updated: 2019/07/23 10:21:52 by ljongman         ###   ########.fr       */
+/*   Updated: 2019/07/23 10:48:39 by ljongman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int		ft_recursive_factorial(int nb)
 	int answer;
 
 	answer = nb;
-	if (nb < 0)	
+	if (nb < 0)
 		return (0);
 	if (nb == 0)
 		return (1);
@@ -32,8 +32,8 @@ int		ft_recursive_factorial(int nb)
 int		main(void)
 {
 	int nb;
-	nb = 10;
+
+	nb = 1;
 	printf("%i\n", ft_recursive_factorial(nb));
-	if (nb < 0)
 	return (0);
 }
